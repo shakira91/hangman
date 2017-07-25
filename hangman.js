363283100d3a9eaf0400aee70efe3e9a2544ba57
Shakira D <Shakira@Shakiras-MacBook-Pro.local>
2017-07-25 07:24:46 -0400

    changes

--- hangman.js
+++ hangman.js
@@ -1,5 +1,21 @@
 "use strict";
 
-var hangmanController = function() {
-	
+var words;
+var game = 0;
+
+var hangman = function() {
+ this.words = words;
+ this.game = game;
 }
+
+hangman.prototype.setUp = function(words, game) {
+ for (var w = 0; w < words[game].length; w++) { 
+ 	console.log(words[game][w])
+ 	$("#guessLetters ul").append("<li class='lines'></li>");
+  }
+  game++;
+}
+
+var newGame = new hangman();
+
+newGame.setUp(words = ["pizza", "corn", "cake", "lovefgd"], game);
