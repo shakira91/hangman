ebe6d3ad8a545b1919216144d90a175507e99731
Shakira D <Shakira@Shakiras-MacBook-Pro.local>
2017-07-26 23:38:48 -0400

    yass

--- hangman.js
+++ hangman.js
@@ -1,49 +1,58 @@
 $(document).ready(function(){ 
+ 
+var Hangman = (function(){
   "use strict";
+   
+  var words = ["HIAS", "refugees", "burundi", "congolese"];
+  var game = 0; //this need to increase IF the person guesses the right answer beforehand.
+  var counter = 5;
 
-var words;
-var game = 0; //this need to increase IF the person guesses the right answer beforehand.
-var counter = 5;
+  var begin = function() {
 
-var Hangman = function(words, game) {
- this.words = words;
- this.game = game;
-}
+    $("#start").on("click", function(){
+      for (var w = 0; w < words[game].length; w++) { 
+        $("#guessLetters ul").append("<li class='lines "+ w +"'></li>");
+      }
+      $("<div id='wordLength'>This word has " + w + " letters.</div>").insertAfter("#guessLetters");
+      $("#letters, #input, label, #submit, #stick").show();
+      $("#start").hide();
+    });
 
-Hangman.prototype.setUp = function() {
-  $("#start").on("click", function(){
-	  for (var w = 0; w < words[game].length; w++) { 
-	    $("#guessLetters ul").append("<li class='lines "+ w +"'></li>");
-	  }
-    $("<div id='wordLength'>This word has " + w + " letters.</div>").insertAfter("#guessLetters");
-    $("#letters, #input, label, #submit").show();
-    $("#start").hide();
-    newGame.guessLetters(words[game]);
-  });
-}
+    return {
+      guess: guessLetters(words[game]), 
+      draw: draw(words[game]) //this needs to be called EACH time a user guesses wrong
+    }
+  }
 
-Hangman.prototype.guessLetters = function(word) {
-	$("#submit").on("click", function() {
-		var userInput = $("#input").val();
-		var index = word.indexOf(userInput);
-		if (word.includes(userInput)) {
-			$(".letter:contains('"+userInput+"')").css( "opacity", "0.5" );
+  var guessLetters = function(word) {
+    $("#submit").on("click", function() {
+      var userInput = $("#input").val();
+      var index = word.indexOf(userInput);
+      if (word.includes(userInput)) {
+        $(".letter:contains('"+userInput+"')").css( "opacity", "0.5" );
 
-			if ($(".lines").hasClass(index)) {
-				console.log(index)
-				$("." + index).html(userInput)
-			}	
-		} else {
-			$("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
-			counter --;
-		}
-	});
+        if ($(".lines").hasClass(index)) {
+          $("." + index).html(userInput)
+        } 
+      } else {
+        $("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
+        counter --;
+      }
+    });
 
- //
-}
+  }
 
-var newGame = new Hangman(words = ["HIAS", "refugees", "burundi", "congolese"], game);
+  var draw = function(stuff) {
+    alert("draw the head");
 
-newGame.setUp();
+  }
+
+  return {
+    begin: begin
+  }
+
+
+}());
+Hangman.begin();
 
 });
