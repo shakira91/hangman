5c364c925045dccb88372aa31334e28b16fe2069
Shakira D <Shakira@Shakiras-MacBook-Pro.local>
2017-07-31 22:56:30 -0400

    blah

--- hangman.js
+++ hangman.js
@@ -1,51 +1,87 @@
-$(document).ready(function(){ 
+$(document).ready(function(){
  
 var Hangman = (function(){
   "use strict";
-   
-  var words = ["HIAS", "refugees", "burundi", "congolese"];
+  
+  var words = ["UNICORNYYYREWW"];
   var game = 0; //this need to increase IF the person guesses the right answer beforehand.
-  var counter = 5;
+  var counter = 6;
+  var userInput;
+
 
   var begin = function() {
 
     $("#start").on("click", function(){
-      for (var w = 0; w < words[game].length; w++) { 
+      for (var w = 0; w < words[game].length; w++) {
         $("#guessLetters ul").append("<li class='lines "+ w +"'></li>");
-      }
+      }   
       $("<div id='wordLength'>This word has " + w + " letters.</div>").insertAfter("#guessLetters");
       $("#letters, #input, label, #submit, #stick").show();
       $("#start").hide();
     });
 
-    return {
-      guess: guessLetters(words[game]), 
-      draw: draw(words[game]) //this needs to be called EACH time a user guesses wrong
-    }
-  }
+      guessLetters(words[game]);
+ 
+  };
 
   var guessLetters = function(word) {
-    $("#submit").on("click", function() {
-      var userInput = $("#input").val();
-      var index = word.indexOf(userInput);
-      if (word.includes(userInput)) {
-        $(".letter:contains('"+userInput+"')").css( "opacity", "0.5" );
 
-        if ($(".lines").hasClass(index)) {
-          $("." + index).html(userInput)
-        } 
+    $("#submit").on("click", function() {
+
+      userInput = $("#input").val().toUpperCase();
+      $("#input").val("");
+
+      var validate = function () {
+        if (/^[A-Z]$/.test(userInput)){
+          return true;
+        } else {
+          alert("please enter a valid letter")
+        }
+      }
+
+      if (userInput && validate) {
+          
+          for (var e = 0; e < words[game].length; e++) {
+              var index = word.indexOf(userInput, e);
+      
+              if (word.includes(userInput)) {
+                $(".letter:contains('"+userInput+"')").css( "opacity", "0.5" );
+                if ($(".lines").hasClass(index)) {
+                  $("." + index).html(userInput);
+                }
+              } else {
+                $("#turnsLeft").show();
+                $("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
+                counter --;
+                checkCount(counter);
+              }
+          }
       } else {
-        $("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
-        counter --;
+        alert("please enter a letter");
       }
     });
 
-  }
+  };
 
-  var draw = function(stuff) {
-    alert("draw the head");
+  var checkCount = function(counter) {
+    if (counter == 5) {
+      alert("draw the head")
+    } else if (counter == 4) {
+      alert("draw the body")
+    } else if (counter == 3) {
+      alert("draw the right arm")
+    } else if (counter == 2) {
+      alert("draw the left arm")
+    } else if (counter == 1) {
+      alert("draw the right leg")
+    } else if (counter == 0) {
+      alert("draw the left leg")
+    } else {
+      alert("awww RIP")
+      $("#turnsLeft").hide();
+    }
 
-  }
+  };
 
   return {
     begin: begin
@@ -54,5 +90,4 @@ var Hangman = (function(){
 
 }());
 Hangman.begin();
-
 });
