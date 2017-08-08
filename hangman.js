a66956997493a9f1e210b38a052996a0cbed7e68
Shakira D <Shakira@Shakiras-MacBook-Pro.local>
2017-08-08 08:30:14 -0400

    hang man

--- hangman.js
+++ hangman.js
@@ -3,14 +3,13 @@ $(document).ready(function(){
 var Hangman = (function(){
   "use strict";
   
-  var words = ["UNICORNYYYREWW"];
+  var words = ["UNI", "yasss"];
   var game = 0; //this need to increase IF the person guesses the right answer beforehand.
   var counter = 6;
   var userInput;
 
 
   var begin = function() {
-
     $("#start").on("click", function(){
       for (var w = 0; w < words[game].length; w++) {
         $("#guessLetters ul").append("<li class='lines "+ w +"'></li>");
@@ -18,13 +17,14 @@ var Hangman = (function(){
       $("<div id='wordLength'>This word has " + w + " letters.</div>").insertAfter("#guessLetters");
       $("#letters, #input, label, #submit, #stick").show();
       $("#start").hide();
-    });
 
-      guessLetters(words[game]);
+      guessLetters(words[game], w);
+
+    });
  
   };
 
-  var guessLetters = function(word) {
+  var guessLetters = function(word, line) {
 
     $("#submit").on("click", function() {
 
@@ -39,48 +39,60 @@ var Hangman = (function(){
         }
       }
 
-      if (userInput && validate) {
-          
-          for (var e = 0; e < words[game].length; e++) {
-              var index = word.indexOf(userInput, e);
-      
-              if (word.includes(userInput)) {
+      if (userInput !== "" && validate()) {
+
+          if (word.includes(userInput)) {
+            for (var e = 0; e < words[game].length; e++) {
+                var index = word.indexOf(userInput, e);
                 $(".letter:contains('"+userInput+"')").css( "opacity", "0.5" );
                 if ($(".lines").hasClass(index)) {
                   $("." + index).html(userInput);
                 }
-              } else {
-                $("#turnsLeft").show();
-                $("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
-                counter --;
-                checkCount(counter);
+              } 
+              line--; console.log(line)
+              if (line == 0) {
+                alert("yay you won. Next game?")
+                $("#turnsLeft").hide();
+                // $("#start").show();
+                //   game++;
               }
+          } else {
+            counter --;
+            $("#turnsLeft").show();
+            $("#turnsLeft").html("<p>You have " + counter + " turns left.</p>");
+            $(".letter:contains('"+userInput+"')").css( "color", "red" );
+            checkCount(counter);
           }
-      } else {
-        alert("please enter a letter");
-      }
+        } else {
+          alert("Please enter a letter");
+        }
     });
-
   };
 
-  var checkCount = function(counter) {
+  var checkCount = function(counter) { 
     if (counter == 5) {
-      alert("draw the head")
-    } else if (counter == 4) {
-      alert("draw the body")
-    } else if (counter == 3) {
-      alert("draw the right arm")
-    } else if (counter == 2) {
-      alert("draw the left arm")
-    } else if (counter == 1) {
-      alert("draw the right leg")
-    } else if (counter == 0) {
-      alert("draw the left leg")
-    } else {
-      alert("awww RIP")
-      $("#turnsLeft").hide();
-    }
+        drawHead();
+      } else if (counter == 4) {
+        alert("draw the body")
+      } else if (counter == 3) {
+        alert("draw the right arm")
+      } else if (counter == 2) {
+        alert("draw the left arm")
+      } else if (counter == 1) {
+        alert("draw the right leg")
+      } else if (counter == 0) {
+        alert("awww RIP")
+        $("#turnsLeft").hide();
+      } 
+  }
+  
 
+  var drawHead = function() {
+    var c = document.getElementById("head");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.stroke();
   };
 
   return {
@@ -90,4 +102,4 @@ var Hangman = (function(){
 
 }());
 Hangman.begin();
-});
+});      
